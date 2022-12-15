e7fe988247d8201cf34f5b5b4a0f68e50f71a2ae
michaellim1231 <michaellim1231@gmail.com>
2022-12-15 08:09:47 +0700

    perubahan index & home dan penambahan asset gambar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,75 +4,20 @@
     <header class="pt-5">
         <div class="container pt-4 pt-xl-5">
             <div class="row pt-5">
-                <div class="col-md-8 text-center text-md-start mx-auto">
-                    <div class="text-center">
-                        <h1 class="display-4 fw-bold mb-5">Cari Buku Cepat, Mudah dan&nbsp;<span class="underline">Lengkap</span>.</h1>
-                        <p class="fs-5 text-muted mb-5">Cari buku dari mana saja, kapan saja dengan real-time</p>
+                <div class="col-md-10 text-left text-md-start mx-auto">
+                    <div class="text-left">
+                        <h1 class="display-4 fw-bold mb-5 text-primary">Cari Buku Cepat, Mudah dan&nbsp;<span class="underline">Lengkap</span>.</h1>
+                        <p class="fs-5 text-muted mb-5">Dapatkan referensi buku bacaan dari mana saja dan kapan saja dengan lengkap serta sumber yang terpercaya</p>
                     </div>
+                    <button type="button" class="btn btn-outline-primary" data-mdb-ripple-color="dark" href="list">Lihat daftar buku</button>
                 </div>
                 <div class="col-12 col-lg-10 mx-auto">
-                    <div class="text-center position-relative"><img class="img-fluid" src="assets/img/illustrations/meeting.svg" style="width: 800px;"></div>
+                    <div class="text-right position-relative"><img class="img-fluid" src="assets/img/brands/573.jpg" style="width: 800px;"></div>
                 </div>
             </div>
         </div>
     </header>
 
-    <section>
-        <div class="container py-4 py-xl-5">
-            <div class="row gy-4 row-cols-1 row-cols-md-2 row-cols-lg-3">
-                <div class="col">
-                    <div class="card border-light border-1 d-flex justify-content-center p-4">
-                        <div class="card-body">
-                            <div class="bs-icon-lg bs-icon-rounded bs-icon-secondary d-flex flex-shrink-0 justify-content-center align-items-center d-inline-block mb-4 bs-icon"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-school">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                    <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6"></path>
-                                    <path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4"></path>
-                                </svg></div>
-                            <div>
-                                <h4 class="fw-bold">Title</h4>
-                                <p class="text-muted">Erat netus est hendrerit, nullam et quis ad cras porttitor iaculis. Bibendum vulputate cras aenean.</p><button class="btn btn-sm px-0" type="button">Learn More&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-arrow-right">
-                                        <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"></path>
-                                    </svg><br></button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card border-light border-1 d-flex justify-content-center p-4">
-                        <div class="card-body">
-                            <div class="bs-icon-lg bs-icon-rounded bs-icon-secondary d-flex flex-shrink-0 justify-content-center align-items-center d-inline-block mb-4 bs-icon"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-school">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                    <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6"></path>
-                                    <path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4"></path>
-                                </svg></div>
-                            <div>
-                                <h4 class="fw-bold">Title</h4>
-                                <p class="text-muted">Erat netus est hendrerit, nullam et quis ad cras porttitor iaculis. Bibendum vulputate cras aenean.</p><button class="btn btn-sm px-0" type="button">Learn More&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-arrow-right">
-                                        <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"></path>
-                                    </svg><br></button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card border-light border-1 d-flex justify-content-center p-4">
-                        <div class="card-body">
-                            <div class="bs-icon-lg bs-icon-rounded bs-icon-secondary d-flex flex-shrink-0 justify-content-center align-items-center d-inline-block mb-4 bs-icon"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-school">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                    <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6"></path>
-                                    <path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4"></path>
-                                </svg></div>
-                            <div>
-                                <h4 class="fw-bold">Title</h4>
-                                <p class="text-muted">Erat netus est hendrerit, nullam et quis ad cras porttitor iaculis. Bibendum vulputate cras aenean.</p><button class="btn btn-sm px-0" type="button">Learn More&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-arrow-right">
-                                        <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"></path>
-                                    </svg><br></button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    
 
 @endsection

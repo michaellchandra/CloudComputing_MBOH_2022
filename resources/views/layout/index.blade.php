9825c5c44c0452d5596c078752eef406a5621aac
Al Jarreau Arman Maulana <hello@shyzago.com>
2022-12-16 08:50:32 +0700

    Update 'listbuku'.

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,52 +9,54 @@
     <title>Home - EverywhereBooks</title>
 
     <!-- Include -->
-    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> --}}
+    {{--
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    --}}
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap">
-    
-    <!-- JavaScript Bundle with Popper -->
-    
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Raleway:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-
-  
-  <nav class="navbar navbar-light navbar-expand-md fixed-top navbar-shrink py-3 bg-primary" id="mainNav">
-    <div class="container"><a class="navbar-brand d-flex align-items-center text-white" href="/"><img class="img-fluid p-1" src="assets/img/brands/34322.png" style="width: 30px;"><span>EverywhereBooks</span></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-        <div class="collapse navbar-collapse " id="navcol-1">
-            <ul class="navbar-nav mx-auto ">
-                <li class="nav-item"><a class="nav-link active text-white" href="/">Home</a></li>
-                <li class="nav-item"><a class="nav-link text-white" href="listbuku">Lists</a></li>
-                <li class="nav-item"><a class="nav-link text-white" href="contact">About</a></li>
-                <a class="btn btn-warning shadow" role="button" href="login">Log In</a>
-            <tr>
-                <td>&nbsp;</td>
-            </tr>
-            <a class="btn btn-warning shadow" role="button" href="signUp">Sign Up</a>
-            </ul>
+    <nav class="navbar navbar-light navbar-expand-md fixed-top navbar-shrink py-3 bg-primary" id="mainNav">
+        <div class="container"><a class="navbar-brand d-flex align-items-center text-white" href="/"><img
+                    class="img-fluid p-1" src="assets/img/brands/34322.png"
+                    style="width: 30px;"><span>EverywhereBooks</span></a><button data-bs-toggle="collapse"
+                class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle
+                    navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse " id="navcol-1">
+                <ul class="navbar-nav mx-auto ">
+                    <li class="nav-item"><a class="nav-link active text-white" href="/">Home</a></li>
+                    <li class="nav-item"><a class="nav-link text-white" href="listbuku">Lists</a></li>
+                    <li class="nav-item"><a class="nav-link text-white" href="contact">About</a></li>
+                    <a class="btn btn-warning shadow" role="button" href="login">Log In</a>
+                    <tr>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <a class="btn btn-warning shadow" role="button" href="signUp">Sign Up</a>
+                </ul>
+            </div>
         </div>
-    </div>
-  </nav>
-    
-  <main class="py-4">
-      @yield('content')
-  </main>
+    </nav>
 
-  <footer>
-    <div class="container py-4 py-lg-5al text-center">
-        <hr>
-        <div class="text-center">
-            <p class="mb-0"><strong>Copyright © 2022</strong> MBOH Team Development</p>
+    <main class="py-4">
+        @yield('content')
+    </main>
+
+    <footer>
+        <div class="container py-4 py-lg-5al text-center">
+            <hr>
+            <div class="text-center">
+                <p class="mb-0"><strong>Copyright © 2022</strong> MBOH Team Development</p>
+            </div>
         </div>
-    </div>
-  </footer>
-  
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-
-<script src="assets/bootstrap/js/startup-modern.js"></script>
-
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+    <script src="assets/bootstrap/js/startup-modern.js"></script>
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,6 @@ Route::get('/listbuku', function () {
 Route::get('/contact', function () {
     return view('contact');
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
